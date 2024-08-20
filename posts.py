92f52436b8c109c681e9774a9960243c4aa2fe42
MBUYt0n <shusrith03@gmail.com>
2024-08-20 17:04:50 +0530

    .

---
+++ posts.py
@@ -0,0 +1,16 @@
+import random
+import time
+import math
+
+class Post:
+    def __init__(self):
+        self.time = time.time()
+        self.likes = 0
+        self.comments = 0
+        self.views = 0
+        self.quality = random.rand()
+
+    def inc_views(self):
+        ma
+
+
