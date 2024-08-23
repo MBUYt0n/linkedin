b033d99112f1ef55e075987393a74af7b4324ca5
MBUYt0n <shusrith03@gmail.com>
2024-08-23 13:02:28 +0530

    .

--- attempt.py
+++ attempt.py
@@ -5,4 +5,24 @@ import random
 g = utils.read_graph()
 p = Post(random.choice([i.user for i in g]))
 g[p.user].add_post(p)
-print("Post added to user", p.user)
+op = p.user
+
+
+def feedback():
+    pass
+
+
+def propagate(centre, level):
+    pass
+
+
+def choose(centre):
+    neighbours = sorted(g[centre].edges, key=lambda x: x[1], reverse=True)
+    k = random.randint(0, (len(neighbours) - 1) // 2)
+    likes = neighbours[:k]
+    k = random.randint(0, (len(neighbours) - 1) // 3)
+    commenter = neighbours[:k]
+
+
+def changes():
+    pass
